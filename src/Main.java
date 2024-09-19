92f7d4524a69ff98de70e70a5123477ccbc524bd
seancleve <seanycleve@gmail.com>
2024-09-19 13:48:11 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        double purchasePrice = 0;
+        double salesTax = 0;
+        final double SALES_TAX_RATE = .05;
+
+        salesTax = purchasePrice * SALES_TAX_RATE;
+
+        System.out.println("");
+
+
+
+    }
+}
